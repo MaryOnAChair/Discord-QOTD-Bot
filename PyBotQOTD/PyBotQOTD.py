bea0fa9d382a0f1b9ccdc33775aa16dd7bb5dc81
MaryInAChair <WorkMayo@outlook.com>
2023-09-06 20:05:36 -0500

    Added run functionality via .env file

--- PyBotQOTD/PyBotQOTD.py
+++ PyBotQOTD/PyBotQOTD.py
@@ -1,12 +1,20 @@
 import string
 import discord
+
 from discord.ext import commands,tasks
-import os
 from dotenv import load_dotenv
-load_dotenv()
+import os
 
-bot_key = os.getenv('dBotKey')
+load_dotenv('.env.txt')
+bot_key = os.getenv('botKey')
 
+CHANNEL_ID = 1146189271691182160
+bot = commands.Bot(command_prefix='$',intents=discord.Intents.all())
 
+@bot.event
+async def on_ready():
+    print("I'm in")
+    print(bot.user)
 
-
+print(bot_key)
+bot.run(bot_key)
