2bec886a94023eae05f88db73aae3bb595d8dce8
MaryInAChair <WorkMayo@outlook.com>
2023-09-16 21:19:53 -0500

    added comments

--- PyBotQOTD/PyBotQOTD.py
+++ PyBotQOTD/PyBotQOTD.py
@@ -8,17 +8,18 @@ from fileHandler import getQOTD,updateUsedQuestions
 
  
 
-load_dotenv('.env.txt')
-bot_key = os.getenv('botKey')
+load_dotenv('.env.txt') # loads .env file
+bot_key = os.getenv('botKey') # gets bot key from .env file
 
-CHANNEL_ID = 1146189271691182160
-bot = commands.Bot(command_prefix='$',intents=discord.Intents.all())
+CHANNEL_ID = 1146189271691182160  # Assigns static channel name, -> Will be changed
+bot = commands.Bot(command_prefix='$',intents=discord.Intents.all()) # Creates bot
 
 
+ # When Bot first starts
 @bot.event
 async def on_ready():
     print("I'm in")
     print(bot.user)
     test = getQOTD()
 
-bot.run(bot_key)
+bot.run(bot_key) # Runs Dave :)
