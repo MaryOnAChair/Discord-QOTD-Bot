a2c256b9698dd2300bd200849b539f280b81c80f
MaryInAChair <WorkMayo@outlook.com>
2023-09-22 17:39:26 -0500

    set up Cog class

--- PyBotQOTD/timeHandler.py
+++ PyBotQOTD/timeHandler.py
@@ -5,7 +5,15 @@ from fileHandler import getQOTD,updateUsedQuestions
 utc = datetime.timezone.utc
 
 # If no tzinfo is given then UTC is assumed.
-time = datetime.time(hour=23, minute=36, tzinfo=utc)
+time = datetime.time(hour=23, minute=36, tzinfo=utc
+
+class MyCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.my_task.start()
+
+    def cog_unload(self):
+        self.my_task.cancel()
 
 
 
