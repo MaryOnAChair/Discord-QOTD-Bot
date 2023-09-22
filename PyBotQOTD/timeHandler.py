0ddb09886aad487ebbfdc99c56cf745a31dc80aa
MaryInAChair <WorkMayo@outlook.com>
2023-09-22 17:42:48 -0500

    Fixed parentheses

--- PyBotQOTD/timeHandler.py
+++ PyBotQOTD/timeHandler.py
@@ -5,7 +5,7 @@ from fileHandler import getQOTD,updateUsedQuestions
 utc = datetime.timezone.utc
 
 # If no tzinfo is given then UTC is assumed.
-time = datetime.time(hour=23, minute=36, tzinfo=utc
+time = datetime.time(hour=23, minute=36, tzinfo=utc)
 
 class MyCog(commands.Cog):
     def __init__(self, bot):
