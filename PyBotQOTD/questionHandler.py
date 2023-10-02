b2c1a6a8e3cd2adf7461b71646c27fa50be46d70
MaryInAChair <WorkMayo@outlook.com>
2023-10-01 22:05:19 -0500

    Formated text file

--- PyBotQOTD/questionHandler.py
+++ PyBotQOTD/questionHandler.py
@@ -1,7 +1,11 @@
 import discord
+from discord.ext import commands
+
 
 qfile = 'listOfSuggestedQuestions.txt'
 def addToSuggested(userSuggestion):
     with open(qfile ,'a' ) as file:
-        file.writelines(userSuggestion)
+        file.writelines(userSuggestion + "\n")
         file.close()
+
+
